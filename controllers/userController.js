ff3ed313d21d4d9fcc2c3ef0212a3a369ddad81e
Md Talha <mdtalha1041@gmail.com>
2021-12-27 18:07:28 +0530

    implemented accessing user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,16 @@
 import asyncHandler from 'express-async-handler';
-
+import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js';
 
+
+//Auth user and get token
+//Acess: Public
+//Route: POST /api/users/login
 const authUser = asyncHandler(async (req, res) => {
   
     const { email, password } = req.body  
-    
-
+  
     const user = await User.findOne({ email })
-
 
     if (user && (await user.matchPassword(password))) {
       res.json({
@@ -16,7 +18,7 @@ const authUser = asyncHandler(async (req, res) => {
         name: user.name,
         email: user.email,
         isAdmin: user.isAdmin,
-        token: null
+        token: generateToken(user._id)
       })
     } else {
       res.status(401)
@@ -24,4 +26,25 @@ const authUser = asyncHandler(async (req, res) => {
     }
   })
 
-  export default authUser
+
+// getting user profile 
+// Acess: Private
+// Route: POST /api/users/profile
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if(user) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      isAdmain: user.isAdmin 
+    })
+  } else {
+     res.status(404)
+     throw new Error('user not found')
+  }
+})
+
+export {authUser, getUserProfile}
+
