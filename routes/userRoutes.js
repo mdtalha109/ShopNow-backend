bdad2c3e873be6e45e6466b9bf9cf721284a4815
Md Talha <mdtalha1041@gmail.com>
2022-03-30 23:01:21 +0530

    added get user routes for admin

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express'
 const router = express.Router()
-import {authUser, getUserProfile} from '../controllers/userController.js'
+
+import {authUser, getUserProfile, registerUser, updateUserProfile, getAllUsers, deleteUser} from '../controllers/userController.js'
+import {protect, isAdmin} from '../middleware/authMiddleware.js'
 
 router.post('/login', authUser)
-router.route('/profile').get(getUserProfile)
+router.route('/').post(registerUser).get(protect, isAdmin, getAllUsers)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+
+router.route('/:id').delete(protect, isAdmin, deleteUser)
 
 
 
