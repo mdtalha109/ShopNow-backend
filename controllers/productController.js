1e0494cd9b8a1cb6b4475fe350449c4dc2bdd998
Md Talha <mdtalha1041@gmail.com>
2022-04-01 23:55:24 +0530

    added product delete function

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,10 +4,9 @@ import APIFeatures from '../utils/apiFeatures.js'
 
 //function for fetching product for product list
 export const getProducts = asyncHandler(async(req, res) => {
-
-    const apiFeatures = new APIFeatures(Product.find(), req.query).search()
-    const products = await apiFeatures.query;
-    
+    const apiFeatures = new APIFeatures(Product.find(), req.query).search().sort()
+    const products = await apiFeatures.query
+    // const products = await Product.find({});
     res.json(products);
 }) 
 
@@ -19,5 +18,39 @@ export const getProductById = asyncHandler(async(req, res) => {
     }
     else {
         res.status(404).json({message: 'Product not found'})
+
+    }
+})
+
+//Fetching product by Category
+export const getProductByCategory = asyncHandler(async(req, res) => {
+    const limit = req.query.Limit
+    let product;
+    if(limit){
+         product = await Product.find({category : req.params.category}).limit(6).sort({price: req.query.sort})
+    }
+    else {
+        product = await Product.find({category : req.params.category}).sort({price: req.query.sort})
+    }
+    
+    if(product){
+        res.json(product);
+    }
+    else {
+        res.status(404).json({message: 'Product not found'})
+    }
+})
+
+
+// Delete Product
+export const deleteProduct = asyncHandler(async(req, res) => {
+    const product = await Product.findById(req.params.id)
+    if(product){
+       await product.remove();
+       res.json({message: 'Product deleted!'})
+    }
+    else {
+        res.status(404).json({message: 'Product not found'})
+
     }
 })
