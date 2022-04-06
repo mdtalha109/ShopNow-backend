f3fce7f6f13dd0e9e350e983aad058f1f4fd496e
Md Talha <mdtalha1041@gmail.com>
2022-04-07 00:18:52 +0530

    feat: add product controller function

--- controllers/productController.js
+++ controllers/productController.js
@@ -54,3 +54,49 @@ export const deleteProduct = asyncHandler(async(req, res) => {
 
     }
 })
+
+
+// create Product
+export const createProduct = asyncHandler(async(req, res) => {
+    const product = new Product({
+        name: 'sample user',
+        price: 0,
+        user: req.user._id,
+        image: './image/sample.jpg',
+        brand: 'sample brand',
+        category: 'sample category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'sample desription'
+    })
+
+    const createdProduct = await product.save()
+    res.status(201).json(createdProduct)
+})  
+
+
+// update Product (put request)
+export const updateProduct = asyncHandler(async(req, res) => {
+    const {name, price, description, image, brand, category, countInStock} = req.body;
+
+    const product = await Product.findById(req.params.id);
+
+    if(product){
+
+        product.name = name;
+        product.price = price;
+        product.description = description;
+        product.image = image;
+        product.brand = brand;
+        product.category = category;
+        product.countInStock = countInStock;
+ 
+        const updatedProduct = await product.save()
+        res.json(updatedProduct)
+    }
+    else{
+        res.status(404);
+        throw new Error('Product not found!!');
+    }
+
+})  
